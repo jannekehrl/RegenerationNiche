9e08ecc16e955d6a72e25c9d75fa1102d33bbf93
jannekehrl <jhrl@uw.edu>
2023-09-15 15:05:12 +0200

    Created initial script for LWF analyses Some thoughts about analyses to do

---
+++ LWF_analyses.R
@@ -0,0 +1,7 @@
+### Analyze LWF data
+## September 15, 2023
+
+## Possible analyses
+# Seedling densities in three years
+# Seedling survival over 2 growing seasons
+# Seedling densities across different LWF stands
